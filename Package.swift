3e92a31c39981c9fd2324de0c1fd649193e0e064
Ulises Giacoman <ulises@joinhandshake.com>
2023-11-15 10:47:05 -0800

    Update checksums

--- Package.swift
+++ Package.swift
@@ -20,12 +20,12 @@ let package = Package(
       .binaryTarget(
         name: "Realm",
         url: "https://github.com/realm/realm-swift/releases/download/v10.44.0/Realm.spm.zip",
-        checksum: "8a4c2afd6ece39d088beaf5dead041c91e1d0778e89a330279625a281412ef7d"
+        checksum: "b26c979ce122acaabb6a6eef9567de43344377afa825c597ae6e14caf508c225"
       ),
       .binaryTarget(
         name: "RealmSwift",
         url: "https://github.com/realm/realm-swift/releases/download/v10.44.0/RealmSwift@15.0.spm.zip",
-        checksum: "3bfeb55fa6603e4ff6bddc764e32abf6bcf87a885abc1ef44785a4f45fbddc22"
+        checksum: "c548dd125df800b5888129f93119360cd3bb3b74ebd740bc21a170f411debee1"
       ),
       .target(name: "_RealmStub"),
     ]
