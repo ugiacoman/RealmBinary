748c82a35e41b4e5244de8dbcf08a3e97e19c273
Ulises Giacoman <ulises@joinhandshake.com>
2023-11-15 10:40:35 -0800

    Update to 10.44

--- Package.swift
+++ Package.swift
@@ -5,26 +5,28 @@ let package = Package(
     name: "RealmBinary",
     platforms: [.iOS(.v12)],
     products: [
-        .library(
-            name: "Realm",
-            targets: ["Realm", "_RealmStub"]),
-        .library(
-            name: "RealmSwift",
-            targets: ["RealmSwift", "Realm", "_RealmStub"])
+      .library(
+        name: "Realm",
+        targets: ["Realm", "_RealmStub"]
+      ),
+      .library(
+        name: "RealmSwift",
+        targets: ["RealmSwift", "Realm", "_RealmStub"]
+      )
     ],
     dependencies: [
     ],
     targets: [
-            .binaryTarget(
-      name: "Realm",
-      url: "https://github.com/realm/realm-swift/releases/download/v10.43.1/Realm.spm.zip",
-      checksum: "8a4c2afd6ece39d088beaf5dead041c91e1d0778e89a330279625a281412ef7d"
-    ),
-    .binaryTarget(
-      name: "RealmSwift",
-      url: "https://github.com/realm/realm-swift/releases/download/v10.43.1/RealmSwift@15.0.spm.zip",
-      checksum: "3bfeb55fa6603e4ff6bddc764e32abf6bcf87a885abc1ef44785a4f45fbddc22"
-    ),
-    .target(name: "_RealmStub"),
+      .binaryTarget(
+        name: "Realm",
+        url: "https://github.com/realm/realm-swift/releases/download/v10.44.0/Realm.spm.zip",
+        checksum: "8a4c2afd6ece39d088beaf5dead041c91e1d0778e89a330279625a281412ef7d"
+      ),
+      .binaryTarget(
+        name: "RealmSwift",
+        url: "https://github.com/realm/realm-swift/releases/download/v10.44.0/RealmSwift@15.0.spm.zip",
+        checksum: "3bfeb55fa6603e4ff6bddc764e32abf6bcf87a885abc1ef44785a4f45fbddc22"
+      ),
+      .target(name: "_RealmStub"),
     ]
 )
