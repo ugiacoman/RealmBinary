825e8cd4cecda1d3d309c83d35713b016ee2d9d9
Ulises Giacoman <ulises@joinhandshake.com>
2023-11-28 13:25:22 -0800

    Fix checksum

--- Package.swift
+++ Package.swift
@@ -20,12 +20,12 @@ let package = Package(
       .binaryTarget(
         name: "Realm",
         url: "https://github.com/realm/realm-swift/releases/download/v10.43.1/Realm.spm.zip",
-        checksum: "b26c979ce122acaabb6a6eef9567de43344377afa825c597ae6e14caf508c225"
+        checksum: "8a4c2afd6ece39d088beaf5dead041c91e1d0778e89a330279625a281412ef7d"
       ),
       .binaryTarget(
         name: "RealmSwift",
         url: "https://github.com/realm/realm-swift/releases/download/v10.43.1/RealmSwift@15.0.spm.zip",
-        checksum: "c548dd125df800b5888129f93119360cd3bb3b74ebd740bc21a170f411debee1"
+        checksum: "3bfeb55fa6603e4ff6bddc764e32abf6bcf87a885abc1ef44785a4f45fbddc22"
       ),
       .target(name: "_RealmStub"),
     ]
