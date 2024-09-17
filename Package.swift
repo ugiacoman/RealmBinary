4e11c5cff02ee8ab9d988d080b43d1193c4d1521
Ulises Giacoman <ulises@joinhandshake.com>
2024-09-16 21:47:22 -0700

    Bump to 10.53.0

--- Package.swift
+++ Package.swift
@@ -19,13 +19,13 @@ let package = Package(
     targets: [
       .binaryTarget(
         name: "Realm",
-        url: "https://github.com/realm/realm-swift/releases/download/v10.45.2/Realm.spm.zip",
-        checksum: "2d7cdd5bb09375e1acd0b9e332f28515667aaa7b78f1b70c945a296f2c10cb5f"
+        url: "https://github.com/realm/realm-swift/releases/download/v10.53.0/Realm.spm.zip",
+        checksum: "632a17f1ca302b777572a20c158f3c52ad76e343cd228887ec7a6a6fba18e9cc"
       ),
       .binaryTarget(
         name: "RealmSwift",
-        url: "https://github.com/realm/realm-swift/releases/download/v10.45.2/RealmSwift@15.1.spm.zip",
-        checksum: "633e386a2987d8f5a25543c8bfc3067ab5f63a373914fe336f74d71e97f09ad9"
+        url: "https://github.com/realm/realm-swift/releases/download/v10.53.0/RealmSwift@15.4.spm.zip",
+        checksum: "0897ce8ceb4f983d6d07ab9b00eb59ab2bc855e2d54018c919d1e92db304f3f1"
       ),
       .target(name: "_RealmStub"),
     ]
